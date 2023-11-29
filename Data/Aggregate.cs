87aa6503f46baeca4b54eb10a89780275e8ee46a
Aditi Chauhan <Aditi@Rahuls-MacBook-Pro.local>
2023-11-30 06:15:01 +1100

    Aggregate of all the numbers function is added

---
+++ Data/Aggregate.cs
@@ -0,0 +1,8 @@
+public class AggregateExample{
+
+    public static List<int> Price = new List<int>{2,3,4 ,5,6,7,4};
+    public static int TotalPrice()
+    {
+        return Price.Aggregate((x,y)=> x+y);
+    }
+}
